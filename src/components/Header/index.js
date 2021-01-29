a7b359e72ab2d6fb084a0a57e8abed6dd8bf87dc
OArtem <artem.ostaschenko@printguard.ru>
2021-01-29 19:30:12 +0300

    Temporaty commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import s from './style.module.css';
 
-const Header = ({ title, descr }) => {
+const Header = ({title, descr}) => {
     return (
         <header className={s.root}>
             <div className={s.forest}></div>
             <div className={s.container}>
-                { title && <h1>{ title }</h1> }
-                { descr && <p>{ descr }</p> }
+                {title && <h1>{title}</h1>}
+                {descr && <p>{descr}</p>}
             </div>
         </header>
     );
---
+++ src/components/PokemonCard/index.js
@@ -0,0 +1,43 @@
+import s from './style.module.css';
+import cardBackSide from './assets/card-back-side.jpg';
+import { useState } from 'react';
+
+const PokemonCard = ({id, name, type, values, img}) => {
+    const [isActive, setActive] = useState(false);
+    const onClickHandler = () => setActive(!isActive);
+    return (
+        <div className={s.root} onClick={onClickHandler}>
+            <div className={`${s.pokemonСard} ${isActive ? s.active : ''}`}>
+                <div className={s.cardFront}>
+                    <div className={`${s.wrap} ${s.front}`}>
+                        <div className={`${s.pokemon} ${s[type]}`}>
+                            <div className={s.values}>
+                                <div className={`${s.count} ${s.top}`}>{values.top}</div>
+                                <div className={`${s.count} ${s.right}`}>{values.right}</div>
+                                <div className={`${s.count} ${s.bottom}`}>{values.bottom}</div>
+                                <div className={`${s.count} ${s.left}`}>{values.left}</div>
+                            </div>
+                            <div className={s.imgContainer}>
+                                <img src={img} alt={name} />
+                            </div>
+                            <div className={s.info}>
+                                <span className={s.number}>#{id}</span>
+                                <h3 className={s.name}>{name}</h3>
+                                <small className={s.type}>Type: <span>{type}</span></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={s.cardBack}>
+                    <div className={`${s.wrap} ${s.back}`}>
+                        <img src={cardBackSide} alt="Сard Backed" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default PokemonCard;
